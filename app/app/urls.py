94323c8eeaa5ac01a93ee3ca8dbfc1fed4461280
Ilyasks95 <84489859+Ilyasks95@users.noreply.github.com>
2025-09-28 14:04:35 +0100

    added health check api

--- app/app/urls.py
+++ app/app/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from core import views as core_views
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -25,6 +26,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/health-check/', core_views.health_check, name='health-check'),
     path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
     path('api/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'),
          name='api-docs'),
